a9ed71bd65e9f8ace409759908d8e61861731863
Alfie Webster <94174590+Awebster10@users.noreply.github.com>
2026-01-27 11:33:24 +0000

    Tweaked doors

--- Assets/Scripts/Doors.cs
+++ Assets/Scripts/Doors.cs
@@ -6,47 +6,32 @@ using TMPro;
 public class Doors : MonoBehaviour
 {
     public bool AtDoor;
-    public bool AtBackDoor;
-    public bool temp;
+    public int DoorValue;
     public GameObject DoorPrompt;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.E)) 
         {
-            temp = true;
-            if(AtDoor == true){SceneManager.LoadScene(1);}
-            if(AtBackDoor == true){SceneManager.LoadScene(0);}
+            if(AtDoor == true){SceneManager.LoadScene(DoorValue);}
         }
     }
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        if(other.tag == "Door")
+        if(other.tag == "Player")
         {
             AtDoor = true;
             DoorPrompt.SetActive(true);
         }
-        if(other.tag == "BackDoor")
-        {
-            AtBackDoor = true;
-            DoorPrompt.SetActive(true);
-        }
+        
     }
-    private void OnTriggerExit2D(Collider2D other) {
-        if(other.tag == "Door")
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        if(other.tag == "Player")
         {
             AtDoor = false;
-            DoorPrompt.SetActive(false);
-        }
-        if(other.tag == "BackDoor")
-        {
-            AtBackDoor = false;
             DoorPrompt.SetActive(false);
         }
     }
