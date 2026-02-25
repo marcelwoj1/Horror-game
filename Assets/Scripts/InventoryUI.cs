8056dc4c642ab8610eb2369c1139153398420a87
marcelwoj1 <marcelwoj.personal2@gmail.com>
2026-02-25 16:37:49 +0000

    Temporary

---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    
+
+    public GameObject[] _ToggleUI;
+
+    private GameObject gameObject;
+
+
+    void Start()
+    {
+        Debug.Log(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
