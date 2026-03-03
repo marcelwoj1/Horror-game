42e97a8ce9abeac699d0ae2b8114761e81c8e546
Alfie Webster <94174590+Awebster10@users.noreply.github.com>
2026-03-03 13:55:52 +0000

    Enemy can move but doesnt follow anchors

--- Assets/Scripts/Enemys/Patrolling Enemy.cs
+++ Assets/Scripts/Enemys/Patrolling Enemy.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class PatrollingEnemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private Vector2 _movementDirection;
+    private Rigidbody2D _rigidBody;
 
-    // Update is called once per frame
-    void Update()
+    private bool IsMovingRight = false;
+
+    private Transform _leftTarget;
+    private Transform _rightTarget;
+
+    public float _moveSpeed;
+
+    public void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _leftTarget = transform.Find("LeftTarget");
+        _rightTarget = transform.Find("RightTarget");
+    }
+    public void Update()
     {
         
+
+        if(transform.position.x > _leftTarget.position.x)
+        {
+            IsMovingRight = false;
+        }
+        if(transform.position.x < _rightTarget.position.x)
+        {
+            IsMovingRight = true;
+        }
+
+        if(IsMovingRight == true)
+        {
+            _movementDirection = (_rightTarget.position - transform.position).normalized;
+            _rigidBody.linearVelocityX = _movementDirection.x * _moveSpeed;
+        }
+        else
+        {
+            _movementDirection = (_leftTarget.position - transform.position).normalized;
+            _rigidBody.linearVelocityX = _movementDirection.x * _moveSpeed;
+        }
+        
     }
+    
 }
