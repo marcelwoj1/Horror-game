985ca0e05e4538fd9a93301012520c32bf6960cf
Alfie Webster <94174590+Awebster10@users.noreply.github.com>
2026-03-05 13:58:44 +0000

    Added spider hurt animation

--- Assets/Scripts/Enemys/Enemy.cs
+++ Assets/Scripts/Enemys/Enemy.cs
@@ -13,6 +13,7 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
+        _animator.Play("Hurt");
         if (health <= 0)
         {
             Die();
