9fe1d732566adfc9e84cbb4a20480b8f29be24f4
Alfie Webster <94174590+Awebster10@users.noreply.github.com>
2026-03-24 16:37:36 +0000

    info displayed when items picked up

--- Assets/Scripts/Inventory/ItemPickup.cs
+++ Assets/Scripts/Inventory/ItemPickup.cs
@@ -4,15 +4,18 @@ public class ItemPickup : MonoBehaviour
 {
     private InvenotryManager inventoryManager;
     public Item item;
+    private ItemInformation itemInformation;
 
     public void Start()
     {
         inventoryManager = GameObject.Find("InventoryManager").GetComponent<InvenotryManager>();
+        itemInformation = GameObject.Find("ItemInformation").GetComponent<ItemInformation>();
     }
 
     public void Pickup()
     {
         inventoryManager.AddItem(item);
         Destroy(gameObject);
+        itemInformation.GetItemInfo(item.name);
     }
 }
